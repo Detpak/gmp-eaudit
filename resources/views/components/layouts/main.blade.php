3cd42e2159661501308ca6f37784de69c6d4b43b
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2022-07-19 17:17:25 +0700

    Add login page

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -5,14 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @auth('admin')
+    {{-- @auth('admin')
     <meta name="api-token" content="{{ session()->get('access_token') }}">
-    @endauth
-    <script src="@/resources/js/app.js"></script>
-    <link rel="stylesheet" href="@/resources/js/logo.png">
+    @endauth --}}
+    @vite(['resources/css/app.css', 'resources/css/app.js'])
     @isset($title)<title>{{ $title }}</title>@endisset
 </head>
 <body>
-    Test
+    {{ $slot }}
 </body>
 </html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+<x-layouts.main title="Login">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col card mt-4">
+                <div class="card-body">
+                    <h4 class="card-title mb-3">Login eAudit</h4>
+                    <div class="mb-3">
+                        <label for="loginEmail" class="form-label">ID</label>
+                        <input type="text" class="form-control" id="loginEmail">
+                    </div>
+                    <div class="mb-3">
+                        <label for="loginEmail" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="loginPassword">
+                    </div>
+                    <button class="btn btn-primary">Login</button>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+</x-layouts.main>
