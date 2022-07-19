8fb9d7f4fb43622d74c7f15f77717bb20ac190cb
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2022-07-19 21:51:41 +0700

    Add login controller

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,24 @@
 <x-layouts.main title="Login">
     <div class="container-fluid">
         <div class="row">
-            <div class="col"></div>
-            <div class="col card mt-4">
-                <div class="card-body">
-                    <h4 class="card-title mb-3">Login eAudit</h4>
-                    <div class="mb-3">
-                        <label for="loginEmail" class="form-label">ID</label>
-                        <input type="text" class="form-control" id="loginEmail">
-                    </div>
-                    <div class="mb-3">
-                        <label for="loginEmail" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="loginPassword">
-                    </div>
-                    <button class="btn btn-primary">Login</button>
+            <div class="col-lg"></div>
+            <div class="col mt-4">
+                <div class="card">
+                    <form class="card-body p-4">
+                        <h4 class="card-title mb-3">Login eAudit</h4>
+                        <div class="mb-3">
+                            <label for="loginEmail" class="form-label">ID</label>
+                            <input type="text" class="form-control" id="loginEmail">
+                        </div>
+                        <div class="mb-3">
+                            <label for="loginEmail" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="loginPassword">
+                        </div>
+                        <button class="btn btn-primary" type="submit">Login</button>
+                    </form>
                 </div>
             </div>
-            <div class="col"></div>
+            <div class="col-lg"></div>
         </div>
     </div>
 </x-layouts.main>
