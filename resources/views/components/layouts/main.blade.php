892ba832963012dd58ce1a9f8d80a354fb69e37f
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2022-07-19 16:41:13 +0700

    Add login files

---
+++ resources/views/components/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @auth('admin')
+    <meta name="api-token" content="{{ session()->get('access_token') }}">
+    @endauth
+    <script src="@/resources/js/app.js"></script>
+    <link rel="stylesheet" href="@/resources/js/logo.png">
+    @isset($title)<title>{{ $title }}</title>@endisset
+</head>
+<body>
+    Test
+</body>
+</html>
