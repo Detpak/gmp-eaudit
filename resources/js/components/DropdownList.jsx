f441a12146824c8911555b21a12b6599583ff156
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2022-10-28 14:31:40 +0700

    Scrolling problem workaround

--- resources/js/components/DropdownList.jsx
+++ resources/js/components/DropdownList.jsx
@@ -61,8 +61,8 @@ export default function DropdownList({ source, selectedItem, setSelectedItem, ca
     };
 
     const handleScroll = (ev) => {
-        const bottomTarget = Math.round(ev.target.scrollHeight - ev.target.scrollTop);
-        if (!isLoading && canFetch && bottomTarget <= Math.round(ev.target.clientHeight)) {
+        const bottomTarget = Math.ceil(ev.target.scrollHeight - ev.target.scrollTop);
+        if (!isLoading && canFetch && bottomTarget == Math.ceil(ev.target.clientHeight)) {
             const nextPage = currentPage + 1;
             fetchData(search, nextPage, true);
         }
