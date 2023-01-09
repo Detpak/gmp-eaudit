0389f411f82ba3a762b652cf7e0d5eb1843454c7
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2023-01-09 11:58:56 +0700

    Fix portal page menu buttons

--- resources/js/layouts/PortalPage.jsx
+++ resources/js/layouts/PortalPage.jsx
@@ -14,9 +14,9 @@ export default function PortalPage() {
                 <Card.Body>
                     {userData ?
                         <div className="vstack gap-2">
-                            {userData.admin_access && <Button href={rootUrl('app')}>Go to Admin Page</Button>}
-                            {userData.auditor && <Button href={rootUrl('audit')}>Submit Audit</Button>}
-                            {userData.auditee && <Button href={rootUrl('corrective-action')}>Create Corrective Action</Button>}
+                            {userData.admin_access ? <Button href={rootUrl('app')}>Go to Admin Page</Button> : <></>}
+                            {userData.auditor ? <Button href={rootUrl('audit')}>Submit Audit</Button> : <></>}
+                            {userData.auditee ? <Button href={rootUrl('corrective-action')}>Create Corrective Action</Button> : <></>}
                             <Button onClick={() => setShowProfileModal(true)}>Edit Profile</Button>
                             <hr />
                             <Button href={rootUrl('deauth')} variant="danger">Logout</Button>
